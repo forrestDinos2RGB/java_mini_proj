a498e2b1eac017b414265e4ba0cadc5b1212ab32
Wen Chien  Yen <wen-chien-yen@Wens-MacBook-Pro.local>
2017-11-09 02:46:49 -0800

    moved tests from MoreBoard to Board

--- proj2/qirkat/MoreBoardTests.java
+++ proj2/qirkat/MoreBoardTests.java
@@ -3,9 +3,32 @@ package qirkat;
 
 import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class MoreBoardTests {
+
+    private static void makeMoves(Board b, String[] moves) {
+        for (String s : moves) {
+            b.makeMove(Move.parseMove(s));
+        }
+    }
+
+    private static final String[] GAME1 =
+            { "c2-c3", "c4-c2",
+                    "c1-c3", "a3-c1",
+                    "c3-a3", "c5-c4",
+                    "a3-c5-c3",
+            };
+
+    private static final String[] GAME2 =
+            { "c2-c3", "c4-c2",
+                    "c1-c3", "a3-c1",
+                    "c3-a3", "c5-c4",
+            };
 
     // the string representation of this is
     // "  b b b b b\n  b b b b b\n  b b - w w\n  w w w w w\n  w w w w w"
@@ -142,6 +165,105 @@ public class MoreBoardTests {
     @Test
     public void testGet() {
         //FIXME
+    }
+
+    @Test
+    public void testUndo2() {
+        Board b0 = new Board();
+        Board b1 = new Board(b0);
+        makeMoves(b0, GAME1);
+        makeMoves(b1, GAME2);
+        b0.undo();
+        assertEquals(b0.toString(), b1.toString());
+    }
+
+    private static int[] initDirection =
+            {0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0
+            };
+
+    @Test
+    public void TestSetDirections1() {
+        Board b0 = new Board();
+        assertTrue(Arrays.equals(initDirection, b0.getDirections()));
+        makeMoves(b0, GAME2);
+        assertTrue(Arrays.equals(initDirection, b0.getDirections()));
+    }
+
+    @Test
+    public void TestSeDirections2() {
+        //FIXME
+    }
+
+    @Test
+    public void testJumpPossible1() {
+        Board b1 = new Board();
+        assertFalse(b1.jumpPossible());
+        String config1 = "wwbwww--ww--wwwb--bbbb-bb";
+        b1.setPieces(config1, PieceColor.WHITE);
+        assertTrue(b1.jumpPossible(14));
+        assertTrue(b1.jumpPossible(18));
+        assertFalse(b1.jumpPossible(1));
+    }
+
+    @Test
+    public void testJumpPossible2() {
+        //FIXME
+        Board b1 = new Board();
+        assertFalse(b1.jumpPossible());
+        String config1 = "---w-w---w-wbw-w---w----w";
+        b1.setPieces(config1, PieceColor.WHITE);
+        b1.makeMoveHelper(Move.parseMove("c3-e3"));
+        assertTrue(b1.jumpPossible(14));
+    }
+
+    @Test
+    public void testValidHorizontal() {
+
+    }
+
+    @Test
+    public void testValidDiagonal() {
+
+    }
+
+    @Test
+    public void testEqualsMove() {
+        Move mov1 = Move.move('a', '1', 'b', '1');
+        Move mov2 = Move.move('a', '1', 'b', '1');
+        assertEquals(mov1, mov2);
+    }
+
+    @Test
+    public void testGetJumpsHelper1() {
+        Board b1 = new Board();
+        String config1 = "---w-w---w--bw-w---w----w";
+        b1.setPieces(config1, PieceColor.BLACK);
+        ArrayList<Move> expected = new ArrayList<>();
+        expected.add(Move.parseMove("c3-e3-e1-c1"));
+        ArrayList<Move> actual = b1.getJumpsHelper(12, b1);
+        for (int i = 0; i < actual.size(); i++) {
+            assertEquals(expected.get(i).toString(), actual.get(i).toString());
+        }
+
+    }
+
+    @Test
+    public void testGetJumpsHelper2() {
+        Board b1 = new Board();
+        String config1 = "---w-w---w-wbw-w---w----w";
+        b1.setPieces(config1, PieceColor.BLACK);
+        ArrayList<Move> expected = new ArrayList<>();
+        expected.add(Move.parseMove("c3-e3-e1-c1"));
+        expected.add(Move.parseMove("c3-a3-a5-a1"));
+        expected.add(Move.parseMove("c3-a3-a5-a1"));
+        ArrayList<Move> actual = b1.getJumpsHelper(12, b1);
+        for (int i = 0; i < actual.size(); i++) {
+            assertEquals(expected.get(i).toString(), actual.get(i).toString());
+        }
     }
 
 
